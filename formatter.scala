96e9e3a6338c0bcae26b39ba6e1635928e7e9b0a
todesking <discommunicative@gmail.com>
2014-10-27 01:24:23 +0900

    improved markdown output

--- formatter.scala
+++ formatter.scala
@@ -1,30 +1,56 @@
 package com.todesking.dox
 
+object Markdown {
+  def code(str:String):String = s"""
+    |```scala
+    |${str}
+    |```
+    |
+    |""".stripMargin
+
+  def inlineCode(str:String):String = s"`${str}`"
+
+  def h(level:Int)(str:String):String = ("#" * level) + " " + str + "\n\n"
+
+  def paragraph(str:String):String =
+    cleanup(str) + "\n\n"
+
+  def link(title:String, url:String):String =
+    s"[${title}](${url})"
+
+  // Seq[(dtContent, ddContent)]
+  def definitionList(items:Seq[(String ,String)]):String =
+    (items.map {case (dt, dd) =>
+      "* " + cleanup(dt) + "\n" + "    * " + cleanup(dd) + "\n"
+    }).mkString("") + "\n"
+
+  private def cleanup(str:String):String =
+    """^\n+|\n+$""".r.replaceAllIn(str, "")
+}
+
 class MarkdownFormatter {
   def format(item:Item, repo:Repository):String = {
     val sb = new scala.collection.mutable.StringBuilder
+    import Markdown._
 
-    def appendln(str:String):Unit = {sb.append(str); sb.append("\n")}
-    def ln():Unit = appendln("")
-
-    appendln(item.id.fullName)
-    appendln(item.signature)
-    item.comment.map(formatMarkup(_)).foreach(appendln(_))
+    sb.append(h(1)(item.id.fullName))
+    sb.append(code(item.signature))
+    item.comment.map(formatMarkup(_)).foreach(sb.append(_))
 
     repo.childrenOf(item).foreach {child =>
       assert(item.id.isParentOf(child.id))
 
-      sb.append(" - ")
-      appendln(child.signature)
-      child.comment.map(formatMarkup(_)).foreach(appendln(_))
+      sb.append(h(2)(inlineCode(child.signature)))
+      child.comment.map(formatMarkup(_)).foreach(sb.append(_))
 
       child match {
         case c:ViaImplicitMethod =>
-          appendln(s"    (added by implicit convertion: ${c.originalId})")
+          sb.append(s" (added by implicit convertion: ${c.originalId})\n")
         case c:ViaInheritMethod =>
-          appendln(s"    (defined at ${c.originalId})")
+          sb.append(s"    (defined at ${c.originalId})\n")
         case _ =>
       }
+      sb.append("\n")
     }
 
     sb.toString
@@ -32,19 +58,21 @@ class MarkdownFormatter {
 
   def formatMarkup(markups:Seq[Markup]):String =
     markups.map(formatMarkup(_)).mkString("")
-  def formatMarkup(markup:Markup):String = markup match {
-    case Markup.Text(content) => content
-    case Markup.Paragraph(children) =>
-      children.map(formatMarkup(_)).mkString("\n\n")
-    case Markup.Dl(items) =>
-      items.map {item =>
-        formatMarkup(item.dt) + "\n" + "    " + formatMarkup(item.dd)
-      } mkString("\n")
-    case Markup.Code(content) =>
-      content.split("\n").map("  " + _).mkString("\n")
-    case Markup.LinkInternal(title, id) =>
-      s"[${title}](${id.fullName})"
-    case Markup.LinkExternal(title, url) =>
-      s"[${title}](${url})"
+
+  def formatMarkup(markup:Markup):String = {
+    import Markdown._
+    markup match {
+      case Markup.Text(content) => content
+      case Markup.Paragraph(children) =>
+        paragraph(children.map(formatMarkup(_)).mkString(""))
+      case Markup.Dl(items) =>
+        definitionList(items.map {item => (formatMarkup(item.dt), formatMarkup(item.dd))})
+      case Markup.Code(content) =>
+        code(content)
+      case Markup.LinkInternal(title, id) =>
+        link(title, id.fullName)
+      case Markup.LinkExternal(title, url) =>
+        link(title, url)
+    }
   }
 }
