5dc4a07825f39ee9fafc44d344cc925ef2097cc3
todesking <discommunicative@gmail.com>
2014-10-29 19:25:23 +0900

    Layout

--- formatter.scala
+++ formatter.scala
@@ -5,10 +5,8 @@ class Layout(optimalWidth:Int, private var indentLevel:Int) {
   val lines = mutable.ArrayBuffer.empty[String]
   var currentLine:String = ""
 
-  def appendLine(str:String):Unit = {
-    appendBreakable0(str)
-    newLine()
-  }
+  override def toString() =
+    lines.mkString("\n") + currentLine + "\n"
 
   def appendText(str:String):Unit = {
     val lines = str.split("\n")
@@ -21,11 +19,16 @@ class Layout(optimalWidth:Int, private var indentLevel:Int) {
   }
 
   def appendUnbreakable(str:String):Unit =
-    str.split("\n").foreach(appendBreakable0(_))
+    str.split("\n").foreach { line => appendUnbreakable0(line); newLine() }
 
   def indent(n:Int):Unit = {
     require(indentLevel + n >= 0)
-    indentLevel += n
+    if(currentLine == " " * indentLevel) {
+      indentLevel += n
+      currentLine = " " * indentLevel
+    } else {
+      indentLevel += n
+    }
   }
 
   def newLine():Unit = {
@@ -34,11 +37,12 @@ class Layout(optimalWidth:Int, private var indentLevel:Int) {
   }
 
   private[this] def appendBreakable0(str:String):Unit = {
-    val words = str.split("""\s+""").filter(_.isEmpty)
+    val words = str.split("""\s+""").filter(_.nonEmpty)
     words.foreach { word =>
       appendUnbreakable0(" " + word)
     }
   }
+
   private[this] def appendUnbreakable0(str:String):Unit = {
     if(currentLine == " " * indentLevel) {
       currentLine += str
@@ -55,112 +59,113 @@ class Layout(optimalWidth:Int, private var indentLevel:Int) {
   }
 }
 
-class Markdown() {
-  def render(markups:Seq[Markup]):String =
-    markups.map(render(_)).mkString("")
+class Markdown(val layout:Layout = new Layout(80, 0)) {
+  def render(markups:Seq[Markup]):Unit =
+    markups.foreach(render(_))
 
-  def render(markup:Markup):String = {
+  def render(markup:Markup):Unit = {
     markup match {
       case Markup.Text(content) =>
-        content
+        layout.appendText(content)
       case Markup.Paragraph(children) =>
-        paragraph(children.map(renderInline(_)).mkString(""))
+        layout.newLine()
+        children.foreach {c => render(c) }
+        layout.newLine()
       case Markup.Dl(items) =>
-        definitionList(items.map {item => (renderInline(item.dt), renderInline(item.dd))})
+        items.foreach {item =>
+          layout.appendText("* ")
+          layout.indent(2)
+          render(item.dt)
+          layout.indent(-2)
+          layout.newLine()
+
+          layout.indent(2)
+          layout.appendText("* ")
+          layout.indent(2)
+          render(item.dd)
+          layout.indent(-2 * 2)
+          layout.newLine()
+        }
       case Markup.Code(content) =>
-        code(content)
+        layout.newLine()
+        layout.appendUnbreakable("```scala")
+        layout.newLine()
+        layout.appendUnbreakable(content)
+        layout.newLine()
+        layout.appendUnbreakable("```")
+        layout.newLine()
+        layout.newLine()
       case Markup.CodeInline(content) =>
-        inlineCode(content)
+        layout.appendUnbreakable("`" + content + "`")
       case Markup.LinkInternal(title, id) =>
         link(title, id.fullName)
       case Markup.LinkExternal(title, url) =>
         link(title, url)
       case Markup.Bold(contents) =>
-        bold(renderInline(contents))
+        layout.appendUnbreakable("*")
+        render(contents)
+        layout.appendUnbreakable("*")
       case Markup.Italic(contents) =>
-        italic(renderInline(contents))
+        layout.appendUnbreakable("_")
+        render(contents)
+        layout.appendUnbreakable("_")
       case Markup.UnorderedList(items) =>
-        unorderedList(items.map{li => renderInline(li.contents)})
+        layout.newLine()
+        items.foreach {item =>
+          layout.appendUnbreakable("* ")
+          layout.indent(2)
+          render(item.contents)
+          layout.indent(-2)
+          layout.newLine()
+        }
     }
   }
 
-  def renderInline(markups:Seq[Markup]):String =
-    markups.map(renderInline(_)).mkString("")
+  def h(level:Int)(str:String):Unit = {
+    layout.appendUnbreakable(("#" * level) + " " + str)
+    layout.newLine()
+    layout.newLine()
+  }
 
-  def renderInline(markup:Markup):String =
-    markup match {
-      case Markup.Paragraph(children) =>
-        renderInline(children)
-      case Markup.Code(content) =>
-        inlineCode(content)
-      case x => render(x)
-    }
+  def h2bar(str:String):Unit = {
+    layout.appendUnbreakable(s" ${str}")
+    layout.newLine()
+    layout.appendUnbreakable(s"${"-" * (str.length + 2)}")
+    layout.newLine()
+    layout.newLine()
+  }
 
-  def code(str:String):String = s"""
-    |```scala
-    |${str}
-    |```
-    |
-    |""".stripMargin
-
-  def inlineCode(str:String):String = s"`${cleanup(str)}`"
-
-  def h(level:Int)(str:String):String = ("#" * level) + " " + str + "\n\n"
-
-  def h2bar(str:String):String =
-    s" ${str}\n${"-" * (str.length + 2)}\n\n"
-
-  def paragraph(str:String):String =
-    cleanup(str) + "\n\n"
-
-  def link(title:String, url:String):String =
-    s"[${title}](${url})"
-
-  // Seq[(dtContent, ddContent)]
-  def definitionList(items:Seq[(String ,String)]):String =
-    (items.map {case (dt, dd) =>
-      "* " + cleanup(dt) + "\n" + "    * " + cleanup(dd) + "\n"
-    }).mkString("") + "\n"
-
-  def unorderedList(items:Seq[String]):String =
-    items.map {item =>
-      s"* ${item}\n"
-    }.mkString
-
-  def bold(content:String) =
-    s" *${content}* "
-
-  def italic(content:String) =
-    s" _${content}_ "
-
-  private def cleanup(str:String):String =
-    """^\n+|\n+$""".r.replaceAllIn(str, "")
+  def link(title:String, url:String):Unit = {
+    layout.appendUnbreakable(s"[${title}](${url})")
+  }
 }
 
 class MarkdownFormatter {
   def format(item:Item, repo:Repository):String = {
-    val sb = new scala.collection.mutable.StringBuilder
     val renderer = new Markdown()
 
-    sb.append(renderer.h(1)(item.id.fullName))
-    sb.append(renderer.code(item.signature))
-    sb.append(renderer.render(item.comment))
+    renderer.h(1)(item.id.fullName)
+    renderer.render(Markup.Code(item.signature))
+    renderer.render(item.comment)
 
     repo.childrenOf(item).sortBy(_.id.fullName).foreach {child =>
-      sb.append('\n')
-      sb.append(renderer.h2bar(renderer.inlineCode(child.signature)))
-      sb.append(renderer.render(child.comment))
+      renderer.layout.newLine()
+      renderer.h2bar("`" + child.signature + "`")
+      renderer.render(child.comment)
 
       child match {
         case c:ViaImplicitMethod =>
-          sb.append(s" (added by implicit convertion: ${c.originalId})\n")
+          renderer.layout.appendUnbreakable(s"(added by implicit convertion: ${c.originalId})")
+          renderer.layout.newLine()
         case c:ViaInheritMethod =>
-          sb.append(s"    (defined at ${c.originalId})\n")
+          renderer.layout.appendUnbreakable(s"(defined at ${c.originalId})")
+          renderer.layout.newLine()
         case _ =>
       }
-      sb.append("\n")
+      renderer.layout.newLine()
     }
 
-    sb.toString
+    renderer.layout.toString()
+
   }
 }
